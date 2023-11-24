901141c6c5b84fd58d4b7d65478d377cb76345d8
Cheshan782 <88943662+Cheshan782@users.noreply.github.com>
2023-11-24 15:21:39 -0500

    Final

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
+var Score_Correct = 0
+var Score_Wrong = 0
+
 // calculate the surface area of a rectangular prism
 function calculateRectangularPrismSurfaceArea(length, width, height) {
   return 2 * (length * width + width * height + length * height);
 }
 
-// calculate the surface area of a triangular prism
-function calculateTriangularPrismSurfaceArea(base, height, length) {
-  return 2 * ((base * length) / 2) + base * length + 2 * ((base * height) / 2);
+// calculate the surface area of a triangular prism                                                                  x                                                             
+function calculateTriangularPrismSurfaceArea(height, length, s1, s2, s3) {
+  return (base * height + (s1 + s2 + s3) * length);
 }
 
 // calculate the surface area of a hexagonal prism
 function calculateHexagonalPrismSurfaceArea(sideLength, height) {
-  return (
-    6 * (((3 * Math.sqrt(3)) / 2) * sideLength * sideLength) +
-    6 * sideLength * height
-  );
+  return (6*sideLength*height+3*1.73205080757*Math.pow(sideLength, 2));
 }
 
-// calculate the surface area of a trapezoidal prism
-function calculateTrapezoidalPrismSurfaceArea(base1, base2, height, length) {
-  return 2 * (0.5 * (base1 + base2) * height + base1 * length + base2 * length);
+// calculate the surface area of a pentagonal prism                                                                      
+function calculatePentagonalPrismSurfaceArea(baseEdgeLength, height, apothem) {
+  return (5 * apothem * baseEdgeLength) + (5 * baseEdgeLength * height);
 }
 
 // Define a function to calculate the surface area of a cone
 function calculateConeSurfaceArea(radius, height) {
-  return Math.PI * radius * (radius + Math.sqrt(radius * radius + height * height));
+  return (Math.PI * radius * (radius + Math.sqrt(radius * radius + height * height)));
 }
 
 // Generate a random question
@@ -32,7 +32,7 @@ function generateQuestion() {
     "Rectangular Prism",
     "Triangular Prism",
     "Hexagonal Prism",
-    "Trapezoidal Prism",
+    "Pentagonal Prism",
     "Cone"
   ];
   const randomShape = shapes[Math.floor(Math.random() * shapes.length)];
@@ -41,7 +41,7 @@ function generateQuestion() {
 
   var imgChanger = document.getElementById('img');
   
-  //-------------------------------------------------------------------------------------------------Rectangle Prism x
+  //-------------------------------------------------------------------------------------------------Rectangle Prism 
   if (randomShape === "Rectangular Prism") {
     const length = Math.floor(Math.random() * 10) + 1;
     const Lengthword = "Length = " + length;
@@ -56,32 +56,40 @@ function generateQuestion() {
     document.getElementById("3").textContent = HeightWord;
 
     document.getElementById("4").textContent = "";
+    document.getElementById("5").textContent = "";
 
     imgChanger.src = "img/Rectangular.png";
     correctAnswer = calculateRectangularPrismSurfaceArea(length, width, height);
     question = 'What is the surface area of this Rectangular Prism?';
 
-  //-------------------------------------------------------------------------------------------------Triangular Prism x
+  //-------------------------------------------------------------------------------------------------Triangular Prism 
   } else if (randomShape === "Triangular Prism") {
-    const base = Math.floor(Math.random() * 10) + 1;
-    const baseWord = "Base = " + base;
-    document.getElementById("1").textContent = baseWord;
 
     const height = Math.floor(Math.random() * 10) + 1;
     const HeightWord = "Height = " + height;
-    document.getElementById("2").textContent = HeightWord;
+    document.getElementById("1").textContent = HeightWord;
 
     const length = Math.floor(Math.random() * 10) + 1;
     const Lengthword = "Length = " + length;
-    document.getElementById("3").textContent = Lengthword;
+    document.getElementById("2").textContent = Lengthword;
 
-    document.getElementById("4").textContent = "";
+    const s1 = Math.floor(Math.random() * 10) + 1;
+    const s1word = "Side 1 = " + s1;
+    document.getElementById("3").textContent = s1word;
+
+    const s2 = Math.floor(Math.random() * 10) + 1;
+    const s2word = "Side 2 = " + s2;
+    document.getElementById("4").textContent = s2word;
+
+    const s3 = Math.floor(Math.random() * 10) + 1;
+    const s3word = "Side 3 = " + s3;
+    document.getElementById("5").textContent = s3word;
 
     imgChanger.src = "img/Triangular.png";
-    correctAnswer = calculateTriangularPrismSurfaceArea(base, height, length);
+    correctAnswer = calculateTriangularPrismSurfaceArea(base, height, length, s1, s2, s3);
     question = 'What is the surface area of this Triangular Prism?';
 
-  //-------------------------------------------------------------------------------------------------Hexagonal Prism x
+  //-------------------------------------------------------------------------------------------------Hexagonal Prism 
   } else if (randomShape === "Hexagonal Prism") {
 
     const sideLength = Math.floor(Math.random() * 10) + 1;
@@ -94,37 +102,38 @@ function generateQuestion() {
 
     document.getElementById("3").textContent = "";
     document.getElementById("4").textContent = "";
+    document.getElementById("5").textContent = "";
 
     imgChanger.src = "img/Hexagonal.png";
     correctAnswer = calculateHexagonalPrismSurfaceArea(sideLength, height);
     question = 'What is the surface area of this Hexagonal Prism?';
 
-  //-------------------------------------------------------------------------------------------------Trapezodial Prism x
-  } else if (randomShape == "Trapezoidal Prism"){
+  //-------------------------------------------------------------------------------------------------Pentagonal Prism 
+  } else if (randomShape == "Pentagonal Prism"){
     //-------------------------------------------------------Base #1
-    const base1 = Math.floor(Math.random() * 10) + 1;
-    const base1Word = "Base 1 = " + base1;
-    document.getElementById("1").textContent = base1Word;
-
-    //-------------------------------------------------------Base #2
-    const base2 = Math.floor(Math.random() * 10) + 1;
-    const base2Word = "Base 2 = " + base2;
-    document.getElementById("2").textContent = base2Word;
+    const baseEdgeLength = Math.floor(Math.random() * 10) + 1;
+    const baseEdgeWord = "Base Edge = " + baseEdgeLength;
+    document.getElementById("1").textContent = baseEdgeWord;
 
     //-------------------------------------------------------Height
     const height = Math.floor(Math.random() * 10) + 1;
     const HeightWord = "Height = " + height;
-    document.getElementById("3").textContent = HeightWord;
+    document.getElementById("2").textContent = HeightWord;
 
-    //-------------------------------------------------------Length
-    const length = Math.floor(Math.random() * 10) + 1;
-    const Lengthword = "Length = " + length;
-    document.getElementById("4").textContent = Lengthword;
+    //-------------------------------------------------------Apothem
+    const apothem = Math.floor(Math.random() * 10) + 1;
+    const ApothemWord = "Apothem = " + apothem;
+    document.getElementById("3").textContent = ApothemWord;
 
-    imgChanger.src = "img/Trapezodial.png";
-    correctAnswer = calculateTrapezoidalPrismSurfaceArea(base1, base2, height, length);
-    question = 'What is the surface area of this Trapezodial Prism?';
-  } else { // Cone
+    document.getElementById("4").textContent = "";
+    document.getElementById("5").textContent = "";
+
+
+    imgChanger.src = "img/Pentagonal.png";
+    correctAnswer = calculatePentagonalPrismSurfaceArea(baseEdgeLength, height, apothem);
+    question = 'What is the surface area of this Pentagonal Prism?';
+   
+  } else { // ----------------------------------------------------------------------------------------------Cone
 
   const radius = Math.floor(Math.random() * 10) + 1;
   const RadiusWord = "Radius = " + radius;
@@ -136,10 +145,14 @@ function generateQuestion() {
 
   document.getElementById("3").textContent = "";
   document.getElementById("4").textContent = "";
+  document.getElementById("5").textContent = "";
+
   imgChanger.src = "img/cone.png";
   correctAnswer = calculateConeSurfaceArea(radius, height);
   question = `Calculate the surface area of a cone`;
   }
+
+  //-----------------------------------------------------------------------------------------------------------Getting Correct Answer
 
   document.getElementById("question").textContent = question;
 
@@ -152,12 +165,13 @@ function generateQuestion() {
 // clears input feild and answer
 function nextQuestion() {
   window.scrollTo(0, 0);
+  stop
   document.getElementById('answer').textContent = '';
   document.getElementById('user-answer').value = '';
   generateQuestion();
 }
 
-// Check the user's answer and give feedback
+// Check the user's answer and give response
 function checkAnswer() {
   window.scrollTo(0, document.body.scrollHeight);
   const userAnswer = parseFloat(document.getElementById("user-answer").value);
@@ -170,10 +184,14 @@ function checkAnswer() {
   var correctAnswerRounded = Math.round(correctAnswer)
 
   if (!isNaN(userAnswer)) {
-    if (userAnswer === correctAnswer) {
+    if (userAnswer === correctAnswerRounded) {
+      Score_Correct = Score_Correct+1
       document.getElementById("answer").textContent = "Correct !!";
+      document.getElementById("score_correct").textContent = Score_Correct;
     } else {
       document.getElementById("answer").textContent = "Incorrect, the answer is: "+ correctAnswerRounded;    
+      Score_Wrong = Score_Wrong+1
+      document.getElementById("score_wrong").textContent = Score_Wrong;
     }
 
   } else {
